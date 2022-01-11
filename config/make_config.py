4118dc326be294938da6bc2a57b99c41d01b7c9d
unonao <naokiuno634@gmail.com>
2022-01-11 10:31:13 +0900

    alphaを変えたpprgoの実験config作成

--- config/make_config.py
+++ config/make_config.py
@@ -1,17 +1,22 @@
 import yaml
 import copy
+import numpy as np
+
+
+def float_representer(dumper, value):
+    text = '{0:.4f}'.format(value)
+    return dumper.represent_scalar(u'tag:yaml.org,2002:float', text)
+
+
+yaml.add_representer(float, float_representer)
 
 data_file_list = ['data/pubmed.npz', 'data/cora_full.npz', 'data/reddit.npz']
-
+min_alpha = 1e-3
+max_alpha = 9e-1
 # for sapprgo
 nsplit_alpha_list = [i for i in range(1, 11)]
 base_dct = yaml.safe_load('''
 data_file: data/pubmed.npz  # Path to the .npz data file
-nsplit_alpha: 2             # Number of PPR matrix
-logspace: False              # split alpha using logspace
-min_alpha: 1e-3
-max_alpha: 9e-1
-#alpha: 0.5                  # PPR teleport probability
 nexp: 5                     # Number of experiments
 seed: 0
 #split_seed: 0               # Seed for splitting the dataset into train/val/test
@@ -35,9 +40,12 @@ patience: 50                # Patience for early stopping
 nprop_inference: 2          # Number of propagation steps during inference
 inf_fraction: 1.0           # Fraction of nodes for which local predictions are computed during inference
 ''')
+base_dct['min_alpha'] = min_alpha
+base_dct['max_alpha'] = max_alpha
 
 file_list = []
 
+# sapprgo
 for data_file in data_file_list:
     for nsplit_alpha in nsplit_alpha_list:
         # logspace is True
@@ -55,6 +63,18 @@ for data_file in data_file_list:
         dct['nsplit_alpha'] = nsplit_alpha
         dct['logspace'] = False
         config_file = 'config/sapprgo_' + data_file.split('/')[-1].split('.')[0] + '_linspace_nsplit' + str(nsplit_alpha).zfill(2) + '.yaml'
+        file_list.append(config_file)
+        with open(config_file, 'w') as f:
+            yaml.dump(dct, f, default_flow_style=False)
+
+# normal
+for data_file in data_file_list:
+    # 1e-3 から 9e-1 まで logspaceで10個のalphaを使う
+    for alpha in np.logspace(np.log10(min_alpha), np.log10(max_alpha), 10):
+        dct = copy.deepcopy(base_dct)
+        dct['data_file'] = data_file
+        dct['alpha'] = float(alpha)
+        config_file = 'config/normal_' + data_file.split('/')[-1].split('.')[0] + f'_alpha{alpha:.4f}' + '.yaml'
         file_list.append(config_file)
         with open(config_file, 'w') as f:
             yaml.dump(dct, f, default_flow_style=False)
